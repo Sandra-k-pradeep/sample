29e129c65c416e45f7802e0ff6c8ce70576ee013
sandra <sandra.inmca2025@saintgits.org>
2024-05-20 15:31:45 +0530

    first commit

---
+++ example.py
@@ -0,0 +1,23 @@
+a='ABCDEF'
+for i in a:
+    print(i)
+    
+for i in range(5):
+    print(i)
+    
+for i in range(1,10,2):
+    print(i)
+    
+for i in range(1,5):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
+
+
+for i in range(1,5):
+    for j in range(i):
+        print('*',end=' ')
+    print()
+
+print(num,'x',i)
